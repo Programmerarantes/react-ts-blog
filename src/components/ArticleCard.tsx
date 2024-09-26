c0eceb3f5930196846119ad34b0b83a90d0a56c8
programmerarantes <programmerarantes@gmail.com>
2024-09-26 10:31:52 -0300

    feat: creating an useeffect to render image

--- src/components/ArticleCard.tsx
+++ src/components/ArticleCard.tsx
@@ -1,36 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Link } from 'react-router-dom';
 import { Article } from '../types/Article';
 
 interface ArticleCardProps {
-   article: Article;
+    article: Article;
 }
 
 export const ArticleCard: React.FC<ArticleCardProps> = ({ article }) => {
 
-    if(!article || !article.attributes) return null   
-    const { title, publishedAt , description, image, category, authorsBio } = article.attributes
+    if (!article || !article.attributes) return null
+
+    const { title, publishedAt, description, image, category, authorsBio } = article.attributes
     const imageUrl = image.data?.attributes?.url
+
+    const [loadedImageUrl, setLoadedImageUrl] = useState<string | null>(null)
+    const [isImageLoading, setIsImageLoading] = useState(true)
+
+    useEffect(() => {
+        if (imageUrl) {
+            const img = new Image()
+            img.src = imageUrl
+            img.onload = () => {
+                setLoadedImageUrl(imageUrl)
+                setIsImageLoading(false)
+            }
+        }
+    }, [imageUrl])
 
 
     return (
         <div className="max-w-4xl mx-auto my-4 p-4 bg-white shadow-md rounded-lg overflow-hidden border border-gray-200">
             <div className="flex flex-col md:flex-row">
                 <div className="md:w-1/3">
-                    <img 
-                        src={`${imageUrl}`}
-                        alt={title} 
-                        className="object-cover w-full h-48 md:h-full"
-                    />
+                    {isImageLoading ? (
+                        <div className="bg-gray-300 h-48 md:h-full animate-pulse"></div>
+                    ) : (
+                        <img
+                            src={loadedImageUrl ?? ''}
+                            alt={title}
+                            className="object-cover w-full h-48 md:h-full"
+                        />
+                    )}
                 </div>
                 <div className="md:w-2/3 p-4">
                     <h1 className="text-xl font-bold">{title}</h1>
-                    <span className='font-bold'>{category.data.attributes.name}</span>
-                    <p className="text-gray-700 my-2">{description.slice(0, 200) + "..."}</p>
-                    <p className="text-gray-500 text-sm">{new Date(publishedAt).toLocaleDateString()} | por: {authorsBio.data.attributes.name}</p>
-                    <Link 
-                        to={`/post/${article.id}`} 
+                    <span className="font-bold">{category.data.attributes.name}</span>
+                    <p className="text-gray-700 my-2">
+                        {description.slice(0, 200) + '...'}
+                    </p>
+                    <p className="text-gray-500 text-sm">
+                        {new Date(publishedAt).toLocaleDateString()} | por: {authorsBio.data.attributes.name}
+                    </p>
+                    <Link
+                        to={`/post/${article.id}`}
                         className="inline-block mt-4 px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600"
                     >
                         Leia Mais
