ba5d984e3b63bf67014cd0d00e11fb45edf7f226
programmerarantes <programmerarantes@gmail.com>
2024-09-26 15:41:14 -0300

    style: styling contactform

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,14 +1,14 @@
 const ContactForm = () => {
   return (
     <div className="container mx-auto px-4 py-16 text-center">
-      <h1 className="text-4xl font-bold mb-8">Entre em contato com nossa equipe</h1>
-      <p className="text-lg mb-12 mx-auto max-w-xl">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim.
+      <h1 className="text-3xl mb-6">Entre em contato com nossa equipe</h1>
+      <p className="text-base mb-12 mx-auto max-w-xl">
+        Se você precisa de apoio durante sua gestação ou parto, entre em contato com nossa equipe de doulas. Estamos aqui para ajudar!
       </p>
-      
-      <form className="mt-12 mx-auto max-w-lg">
+
+      <form className="mt-12 mx-auto max-w-lg text-left">
         <div className="mb-6">
+        <label  className="block mb-2 text-sm font-medium text-gray-600">Nome:</label>
           <input
             type="text"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -16,6 +16,7 @@ const ContactForm = () => {
           />
         </div>
         <div className="mb-6">
+          <label className="block mb-2 text-sm font-medium text-gray-600 ">Email:</label>
           <input
             type="email"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -23,30 +24,30 @@ const ContactForm = () => {
           />
         </div>
         <div className="mb-6">
-          <select
+          <label className="block mb-2 text-sm font-medium text-gray-600 ">Assunto:</label>
+          <input
+            type="text"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          >
-            <option value="">Doula</option>
-            <option value="general">Yoga para Gestante</option>
-            <option value="support">Consultoria Amamentação</option>
-            <option value="feedback">Uso da placenta</option>
-            <option value="feedback">Terapia integrativa</option>
-          </select>
+            placeholder="Assunto"
+          />
         </div>
         <div className="mb-6">
+          <label className="block mb-2 text-sm font-medium text-gray-600 ">Mensagem:</label>
           <textarea
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className="shadow appearance-none border rounded w-full py-4 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Mensagem"
-            
+
             style={{ resize: 'none' }}
           ></textarea>
         </div>
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-        >
-          Enviar Mensagem
-        </button>
+        <div className="text-center">
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Enviar Mensagem
+          </button>
+        </div>
       </form>
     </div>
   );
